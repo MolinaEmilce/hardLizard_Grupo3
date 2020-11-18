d39e92cc6285a88ec932bf936c7639bf97ac036f
EliMC-Nag <eliana.naguila@gmail.com>
2020-11-18 13:23:01 -0200

    se agrego para acceder /las paginas

--- router.js
+++ router.js
@@ -1,15 +1,35 @@
-const index = require('./src/index')
+const index = require('./src/index');
 
-
-module.exports = function(req,res){
+module.exports=function(req,res){
     switch (req.url) {
-        case '/':
-            index.homePage(req,res)
-            break;
-        case '/en-cartelera':
-            index.enCartelera(req,res)
-            break
-        default:
-            break;
-    }
+		
+        case '/': //
+             index.homePage(req,res)
+			 break;
+		
+        case '/en-cartelera': //
+             index.enCartelera(req,res)
+			 break;
+
+        case '/mas-votadas': //
+             index.masVotadas(req,res)		
+			 break;
+
+        case '/sucursales': //		    
+             index.sucursales(req,res)
+			 break;
+
+        case '/contacto': 
+             index.contacto(req,res)		    
+			 break;
+
+        case '/preguntas-frecuentes': 
+             index.preguntasFrecuentes(req,res)			
+			 break;
+		default:
+
+			res.end('404 not found')
+
+	}
+
 }
